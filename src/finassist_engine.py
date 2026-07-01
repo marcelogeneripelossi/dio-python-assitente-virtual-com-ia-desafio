8519aa1a176574a136944ee805c82cd9a0c53daf
Marcelo Generi Pelossi <marcelo.pelossi@hotmail.com>
2026-07-01 12:53:51 -0300

    Update finassist_engine.py - arquivos modularizados
    Update finassist_engine.py para modularização de retinas
    - criados arquivos para separação em módulos:
      - consultas_cliente.py
      - contexto.py
      - util.py
    - acertos e melhorias

--- src/finassist_engine.py
+++ src/finassist_engine.py
@@ -1,4 +1,4 @@
-## Código a ser utilizado no Colab. No Notebook são gerados os arquivos de dados para utilização da simulação do Agente.
+## Código para testes do Motor do Finassist. Pode ser executado no VS Code ou via CLI. Veja o item '3. Interface de Linha de Comando (CLI)' na seção 'Como Executar' do README.
 ## Para simulação utilizando uma interface simples com Streamlit, é utilizado o app.py
 ## Versão: lê os dados já gravados em arquivos na pasta 'data/'
 
@@ -6,12 +6,19 @@ import csv
 import json
 import pandas as pd
 import random
-import re
-import unicodedata
 
 import os
-import subprocess
 
+import consultas_cliente
+import contexto
+from util import (
+    cls,
+    detectar_intencao,
+    normalizar_perfil,
+    normalizar_texto,
+    termo_negado,
+    tokenizar
+)
 
 # system_prompt = """
 # Você é o Finassist, um assistente virtual com IA especializado em educação financeira e planejamento de metas. Seu público-alvo são clientes que buscam organizar suas finanças de forma consciente.
@@ -33,6 +40,7 @@ import subprocess
 # ==========================================================
 # Configurações
 # ==========================================================
+
 BASE_DIR = os.path.dirname(
     os.path.abspath(__file__)
 )
@@ -48,70 +56,6 @@ DATA_DIR = os.path.abspath(
 )
 
 # ==========================================================
-# Contexto global
-# ==========================================================
-
-perfis = None
-produtos = None
-transacoes = None
-historico = None
-palavras_chave = None
-chave_sugestoes = None
-
-# ==========================================================
-# Constantes
-# ==========================================================
-
-mapa_risco_perfil = {
-    "baixo": "conservador",
-    "medio": "moderado",
-    "alto": "arrojado"
-}
-
-negacoes = {
-    "dispensar",
-    "dispenso",
-    "forma alguma",
-    "evitar",
-    "jamais",
-    "nada",
-    "nao",
-    "nem",
-    "nem pensar",
-    "nem quero",
-    "nem sequer",
-    "nunca"}
-
-perfil_sinonimos = {
-    "arriscado": "arrojado",
-    "risco": "arrojado",
-    "acoes": "arrojado",
-    "ação": "arrojado",
-    "acao": "arrojado",
-    "bolsa": "arrojado",
-
-    "seguro": "conservador",
-    "segurança": "conservador",
-
-    "equilibrado": "moderado"
-}
-
-perfil_tipos = [
-        "conservador",
-        "conservadora",
-        "conservadores",
-        "conservadoras",
-        "moderado",
-        "moderada",
-        "moderados",
-        "moderadas",
-        "arrojado",
-        "arrojada",
-        "arrojados",
-        "arrojadas"
-    ]
-
-# ==========================================================
 # Inicialização
 # ==========================================================
 
@@ -123,12 +67,19 @@ def inicializar_contexto(dados):
     global chave_sugestoes
     global perfis
 
-    produtos = dados["produtos"]
-    transacoes = dados["transacoes"]
-    historico = dados["historico"]
-    palavras_chave = dados["palavras_chave"]
-    chave_sugestoes = dados["chave_sugestoes"]
-    perfis = dados["perfis"]
+    contexto.produtos = dados["produtos"]
+    contexto.transacoes = dados["transacoes"]
+    contexto.historico = dados["historico"]
+    contexto.palavras_chave = dados["palavras_chave"]
+    contexto.chave_sugestoes = dados["chave_sugestoes"]
+    contexto.perfis = dados["perfis"]
+
+    produtos = contexto.produtos
+    transacoes = contexto.transacoes
+    historico = contexto.historico
+    palavras_chave = contexto.palavras_chave
+    chave_sugestoes = contexto.chave_sugestoes
+    perfis = contexto.perfis
 
 # ============================================================
 # Carregamento dos dados
@@ -183,259 +134,8 @@ def carregar_dados():
     }
 
 # ============================================================
-# Utilidades
-# ============================================================
-
-def cls():
-    comando = 'cls' if os.name == 'nt' else 'clear'
-    subprocess.run (comando, shell = True)
-
-def formatar_moeda(valor):
-    return (
-        f"{valor:,.2f}"
-        .replace(",", "_")
-        .replace(".", ",")
-        .replace("_", ".")
-    )
-
-def normalizar_texto(texto):
-    texto = texto.lower()
-
-    texto = unicodedata.normalize(
-        "NFD",
-        texto
-    )
-
-    texto = "".join(
-        c
-        for c in texto
-        if unicodedata.category(c) != "Mn"
-    )
-
-    return texto
-
-def normalizar_perfil(perfil):
-    perfil = perfil.lower().strip()
-
-    mapa = {
-        "conservador": "conservador",
-        "conservadora": "conservador",
-        "conservadores": "conservador",
-        "conservadoras": "conservador",
-
-        "moderado": "moderado",
-        "moderada": "moderado",
-        "moderados": "moderado",
-        "moderadas": "moderado",
-
-        "arrojado": "arrojado",
-        "arrojada": "arrojado",
-        "arrojados": "arrojado",
-        "arrojadas": "arrojado"
-    }
-
-    return mapa.get(perfil, perfil)
-
-def tokenizar(texto):
-    texto = normalizar_texto(texto)
-
-    return re.findall(
-        r'\b[\w/]+\b',
-        texto.lower()
-    )
-
-def termo_negado(tokens, indice, janela=4):
-    """
-    Verifica se existe uma negação nas palavras imediatamente anteriores ao termo.
-
-    Suporta:
-    - unigramas: "não", "nunca", "jamais"
-    - bigramas: "nem pensar", "forma alguma"
-    """
-
-    inicio = max(0, indice - janela)
-
-    trecho = tokens[inicio:indice]
-
-    # unigramas
-    for token in trecho:
-
-        if token in negacoes:
-            return True
-
-    # bigramas
-    for i in range(len(trecho) - 1):
-
-        expressao = f"{trecho[i]} {trecho[i + 1]}"
-
-        if expressao in negacoes:
-            return True
-
-    return False
-
-def detectar_intencao(pergunta, chave):
-
-    pergunta_normalizada = normalizar_texto(
-        pergunta
-    )
-
-    termos = palavras_chave.get(
-        chave,
-        []
-    )
-
-    return any(
-        normalizar_texto(termo)
-        in pergunta_normalizada
-        for termo in termos
-    )
-
-# ============================================================
 # Motor do Finassist 
 # ============================================================
-# ===================================
-# Métodos do Motor (início)
-# ===================================
-
-def obter_gastos_maior_categoria(perfil):
-
-        saidas = transacoes[
-            (transacoes["id_cliente"] == perfil["id"])
-            &
-            (transacoes["tipo"] == "saida")
-        ]
-
-        if not saidas.empty:
-
-            resumo = (
-                saidas
-                .groupby("categoria")["valor"]
-                .sum()
-            )
-
-            categoria = resumo.idxmax()
-
-            valor = resumo.max()
-
-            return (
-                f"Sua maior despesa está na categoria "
-                f"'{categoria}', totalizando "
-                f"R$ {formatar_moeda(valor)}."
-            )
-
-def obter_gastos(perfil):
-        saidas = transacoes[
-            (transacoes["id_cliente"] == perfil["id"])
-            &
-            (transacoes["tipo"] == "saida")
-        ]
-
-        total = saidas["valor"].sum()
-
-        return(
-            f"Suas despesas totalizam "
-            f"R$ {formatar_moeda(total)}."
-        )
-
-def obter_historico(perfil):
-    hist = historico[
-        historico["id_cliente"] == perfil["id"]
-    ]
-
-    if hist.empty:
-        return "Você não possui atendimentos registrados."
-
-    hist = hist.sort_values("data", ascending=False)    #.head(5)
-
-    linhas = []
-
-    for _, atendimento in hist.iterrows():
-
-        linhas.append(
-            f"- {atendimento['data']} | "
-            f"{atendimento['tema']} | "
-            f"{atendimento['canal']}"
-        )
-
-    return (
-        "Seu histórico de atendimento:\n"
-        + "\n".join(linhas)
-    )
-
-def obter_metas(perfil):
-    metas = perfil.get("metas", [])
-
-    if not metas:
-        return ("Você não possui metas cadastradas.")
-
-    if metas:
-        texto_metas = []
-
-        for meta in metas:
-            texto_metas.append(
-                f"- {meta['meta']} "
-                f"(R$ {formatar_moeda(meta['valor_necessario'])} "
-                f"até {meta['prazo']})"
-            )
-
-        return (
-            "Suas metas atuais são:\n"
-            + "\n".join(texto_metas)
-        )
-
-def obter_transacoes(perfil):
-        trans = transacoes[
-            transacoes["id_cliente"] == perfil["id"]
-        ]
-
-        if not trans.empty:
-
-            ultimas = (
-                trans
-                .sort_values("data", ascending=False)
-                .head(3)
-            )
-
-            linhas = []
-
-            for _, t in ultimas.iterrows():
-
-                linhas.append(
-                    f"- {t['data']} | "
-                    f"{t['descricao']} | "
-                    f"R$ {formatar_moeda(t['valor'])}"
-                )
-
-            return (
-                "Suas últimas transações foram:\n"
-                + "\n".join(linhas)
-            )
-
-
-def obter_ultimo_atendimento(perfil):
-    hist = historico[
-        historico["id_cliente"] == perfil["id"]
-    ]
-
-    if hist.empty:
-        return "Você não possui atendimentos registrados."
-
-    ultimo = (
-        hist
-        .sort_values("data")
-        .iloc[-1]
-    )
-
-    return (
-        f"Seu último atendimento foi em "
-        f"{ultimo['data']} via {ultimo['canal']}.\n"
-        f"Tema: {ultimo['tema']}.\n"
-        f"Resumo: {ultimo['resumo']}."
-    )
-
-# ===================================
-# Métodos do Motor (fim)
-# ===================================
 
 def motor_finassist(perfil, pergunta, dados):
 
@@ -465,7 +165,7 @@ def motor_finassist(perfil, pergunta, dados):
     )
 
     if quer_metas:
-        resposta = obter_metas(perfil)
+        resposta = consultas_cliente.obter_metas(perfil)
         resposta_final.append(resposta)
 
     # ========================================================
@@ -482,7 +182,7 @@ def motor_finassist(perfil, pergunta, dados):
     )
 
     if quer_ultimo_atendimento:
-        resposta = obter_ultimo_atendimento(perfil)
+        resposta = consultas_cliente.obter_ultimo_atendimento(perfil)
         resposta_final.append(resposta)
 
     #=====================
@@ -495,7 +195,7 @@ def motor_finassist(perfil, pergunta, dados):
     )
 
     if quer_historico:
-        resposta = obter_historico(perfil)
+        resposta = consultas_cliente.obter_historico(perfil)
         resposta_final.append(resposta)
 
     
@@ -513,7 +213,7 @@ def motor_finassist(perfil, pergunta, dados):
     )
 
     if quer_transacoes:
-        resposta = obter_transacoes(perfil)
+        resposta = consultas_cliente.obter_transacoes(perfil)
         resposta_final.append(resposta)
 
 
@@ -527,7 +227,7 @@ def motor_finassist(perfil, pergunta, dados):
     )
 
     if quer_gastos:
-        resposta = obter_gastos(perfil)
+        resposta = consultas_cliente.obter_gastos(perfil)
         resposta_final.append(resposta)
 
     #=======
@@ -540,7 +240,7 @@ def motor_finassist(perfil, pergunta, dados):
     )
 
     if quer_categoria_maior_gasto:
-        resposta = obter_gastos_maior_categoria(perfil)
+        resposta = consultas_cliente.obter_gastos_maior_categoria(perfil)
         resposta_final.append(resposta)
 
     # ========================================================
@@ -588,7 +288,7 @@ def motor_finassist(perfil, pergunta, dados):
 
                 linhas.append(
                     f"- Risco {risco_texto} "
-                    f"(perfil {mapa_risco_perfil[risco_json]}): "
+                    f"(perfil {contexto.mapa_risco_perfil[risco_json]}): "
                     + ", ".join(nomes_limitados)
                     + "."
                 )
@@ -605,7 +305,7 @@ def motor_finassist(perfil, pergunta, dados):
     perfil_detectado = None
     perfil_negado = None
 
-    for termo in perfil_tipos:
+    for termo in contexto.perfil_tipos:
         termo_busca = termo.lower()
 
         for indice, token in enumerate(tokens):
@@ -637,7 +337,7 @@ def motor_finassist(perfil, pergunta, dados):
 
     if perfil_detectado is None and perfil_negado is None:
 
-        for termo, perfil_associado in perfil_sinonimos.items():
+        for termo, perfil_associado in contexto.perfil_sinonimos.items():
 
             if termo not in tokens:
                 continue
@@ -664,7 +364,7 @@ def motor_finassist(perfil, pergunta, dados):
 
     quer_investimento = any(
         termo in pergunta_normalizada
-        for termo in palavras_chave.get(
+        for termo in contexto.palavras_chave.get(
             "investir",
             []
         )
@@ -779,7 +479,7 @@ def motor_finassist(perfil, pergunta, dados):
 
     sugestoes = []
 
-    for chave, termos in palavras_chave.items():
+    for chave, termos in contexto.palavras_chave.items():
 
         encontrou_chave = False
 
@@ -806,10 +506,10 @@ def motor_finassist(perfil, pergunta, dados):
             continue
 
         # chave direta
-        if chave in chave_sugestoes:
+        if chave in contexto.chave_sugestoes:
 
             sugestoes.extend(
-                chave_sugestoes[chave]
+                contexto.chave_sugestoes[chave]
             )
 
     # remove duplicidades
@@ -854,7 +554,7 @@ def rodar_interacao(dados):
         print("\nClientes cadastrados:")
 
         for p in sorted(
-            perfis,
+            contexto.perfis,
             key=lambda x: x["nome"]
         ):
 
@@ -872,7 +572,7 @@ def rodar_interacao(dados):
 
       encontrados = [
           p
-          for p in perfis
+          for p in contexto.perfis
           if nome.lower()
           in p["nome"].lower()
       ]
